2ae52f3937f63aec67a080db0b50fcef3e31d6f2
Matheus-kb <matheuskbitencourt@gmail.com>
2024-09-20 17:59:33 -0300

    feat: configuration for tailwind and component lib

--- app/App.tsx
+++ app/App.tsx
@@ -1,12 +1,16 @@
-import { Text, View } from 'react-native';
-import Profile from './src/Profile';
+import { Text, View } from "react-native";
+import Profile from "./src/Profile";
+import { Card, CardContent } from "./src/components/Card";
 
 export default function App() {
   return (
-    <View className='flex-1 items-center justify-center'>
-      <Text>App</Text>
-      <Profile />
+    <View className="flex-1 items-center justify-center bg-slate-200">
+      <Card className="bg-white w-48 h-16 rounded-sm">
+        <CardContent>
+          <Text className="font-bold">App</Text>
+          <Profile />
+        </CardContent>
+      </Card>
     </View>
   );
 }
-
--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,9 +1,22 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./App.{js,jsx,ts,tsx}", "./<src>/**/*.{js,jsx,ts,tsx}"],
+  content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#1E40AF',  // Azul Profundo
+        secondary: '#F97316',  // Laranja Quente
+        accent: '#22C55E',  // Verde Vibrante
+        background: '#F3F4F6',  // Cinza Claro
+        surface: '#FFFFFF',  // Branco Puro
+        error: '#EF4444',  // Vermelho Vibrante
+        warning: '#F59E0B',  // Amarelo Alerta
+        info: '#3B82F6',  // Azul Informativo
+        success: '#10B981',  // Verde Sucesso
+        muted: '#9CA3AF',  // Cinza Suave
+        textPrimary: '#111827',  // Preto Escuro
+        textSecondary: '#6B7280',  // Cinza Médio
+      },
+    },
   },
   plugins: [],
-}
-
+};
