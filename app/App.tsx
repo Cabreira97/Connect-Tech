1f8bf2f39909832d3db3fffbda0b2fa332b833d8
Matheus-kb <matheuskbitencourt@gmail.com>
2024-09-21 19:30:18 -0300

    feat: Register client page

--- app/App.tsx
+++ app/App.tsx
@@ -2,11 +2,12 @@ import { Text, View } from "react-native";
 import InitialPage from "./src/screens/Initial";
 import LoginPage from "./src/screens/Login";
 import CreateTypeSelector from "./src/screens/CreateTypeSelector";
+import RegisterClient from "./src/screens/RegisterClient";
 
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-slate-200">
-      <CreateTypeSelector />
+      <RegisterClient />
     </View>
   );
 }
