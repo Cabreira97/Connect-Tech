260c71e166bbe1435b015723c2ad5ea0d2b475f4
Matheus-kb <matheuskbitencourt@gmail.com>
2024-09-21 19:21:32 -0300

    feat: Type account selector for the register profile

--- app/App.tsx
+++ app/App.tsx
@@ -1,11 +1,12 @@
 import { Text, View } from "react-native";
 import InitialPage from "./src/screens/Initial";
 import LoginPage from "./src/screens/Login";
+import CreateTypeSelector from "./src/screens/CreateTypeSelector";
 
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-slate-200">
-      <LoginPage />
+      <CreateTypeSelector />
     </View>
   );
 }
